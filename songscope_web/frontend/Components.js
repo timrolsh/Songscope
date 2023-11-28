c5cc0759bfb4f20c78235eb44a5c5697d35f7741
Tim Rolshud <tim.rolsh@gmail.com>
2023-11-28 10:20:29 -0500

    switch stack to next

--- songscope_web/frontend/Components.js
+++
@@ -1,2 +0,0 @@
-import {HelloComponent} from "./components/Test.js";
-customElements.define("hello-component", HelloComponent);
--- songscope_web/frontend/components/Test.js
+++
@@ -1,8 +0,0 @@
-import {LitElement, html} from "lit";
-
-export class HelloComponent extends LitElement {
-    render() {
-        return html` <div>Hello</div> `;
-    }
-}
-
