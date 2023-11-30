72ca4638692ff1a1dcca252216b7e51ebfbcc2db
Tim Rolshud <tim.rolsh@gmail.com>
2023-11-30 10:44:50 -0500

    move code to work with mysql instead of pg

--- songscope_web/src/server/start_server.js
+++ songscope_web/src/server/start_server.js
@@ -9,24 +9,26 @@ const nextApp = next({dev: devMode});
 nextApp.prepare().then(() => {
     const server = express();
     // Establish database connection
-    db.query("select NOW();")
-        .then(() => {
-            // Handle both get and post requests with Next.js API system
-            server.get("*", (request, response) => {
-                return nextApp.getRequestHandler()(request, response);
-            });
-            server.post("*", (request, response) => {
-                return nextApp.getRequestHandler()(request, response);
-            });
-            server.listen(port, (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log(`SONGSCOPE: Started server on http://localhost:${port}`);
-            });
-        })
-        .catch((err) => {
-            console.error("SONGSCOPE: Unable to start server. Failed to connect to database", err);
+    db.query("select NOW();", (error, results, fields) => {
+        if (error) {
+            console.error(
+                "SONGSCOPE: Unable to start server. Failed to connect to database",
+                error
+            );
             process.exit(1);
+        }
+        // Handle both get and post requests with Next.js API system
+        server.get("*", (request, response) => {
+            return nextApp.getRequestHandler()(request, response);
         });
+        server.post("*", (request, response) => {
+            return nextApp.getRequestHandler()(request, response);
+        });
+        server.listen(port, (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`SONGSCOPE: Started server on http://localhost:${port}`);
+        });
+    });
 });
