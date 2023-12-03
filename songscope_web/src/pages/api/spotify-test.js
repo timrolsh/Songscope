4d99276926029789c626949f8cb9e95b72473585
Tim Rolshud <tim.rolsh@gmail.com>
2023-12-03 13:54:23 -0500

    make route functional without query string

--- songscope_web/src/pages/api/spotify-test.js
+++ songscope_web/src/pages/api/spotify-test.js
@@ -1,32 +1,34 @@
 // Test route for plumbing project, delete later
-const qs = require("qs");
 require("dotenv").config();
 
 const client_id = process.env.SPOTIFY_CLIENT_ID;
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET;
 const auth_token = Buffer.from(`${client_id}:${client_secret}`).toString("base64");
 
-const getAuth = async () => {
-    const token_url = "https://accounts.spotify.com/api/token";
-    const data = qs.stringify({ 'grant_type': 'client_credentials' });
-    const response = await fetch(token_url, {
-        method: 'POST',
-        headers: {
-            'Authorization': `Basic ${auth_token}`,
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: data
-    });
-    const json = await response.json();
-    return json.access_token;
+function toUrlEncoded(obj) {
+    return Object.keys(obj)
+        .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(obj[key]))
+        .join("&");
 }
+
+const getAuth = async () => {
+    const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
+        headers: {
+            "Authorization": `Basic ${auth_token}`,
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: toUrlEncoded({grant_type: "client_credentials"})
+    });
+    return JSON.parse(await response.text()).access_token;
+};
 
 export default async (request, response) => {
     try {
         let access_token = await getAuth();
         const result = await fetch("https://api.spotify.com/v1/albums/2QRedhP5RmKJiJ1i8VgDGR", {
             headers: {
-                Authorization: `Bearer ${access_token}`,
+                Authorization: `Bearer ${access_token}`
             }
         });
         let albumArtUrl = await result.json();
