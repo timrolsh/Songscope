35d4b3520ecb274347acd6516057e786b5397b86
krisharora <43799189+kna27@users.noreply.github.com>
2023-12-04 09:02:29 -0500

    Add DB Write test (#29)

---
+++ songscope_web/src/components/DBWriteTesterButton.jsx
@@ -0,0 +1,32 @@
+export default () => {
+    return (
+        <>
+            <input type="number" id="rating" name="rating" min="1" max="5" placeholder="Rating (1-5)" className="text-black"></input>
+            <br></br>
+            <button
+                onClick={() => {
+                    fetch("/api/db-write-test", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            spotify_work_id: "2QRedhP5RmKJiJ1i8VgDGR",
+                            rating: document.getElementById("rating").value,
+                            user_id: 0
+                        })
+                    }).then((response) => {
+                        response.text().then((text) => {
+                            alert(`Database write test successful: ${text}`);
+                        });
+                    })
+                        .catch((err) => {
+                            alert(`Database write test unsuccessful. Error: ${err}`);
+                        });
+                }}
+            >
+                Create Rating for "Playboi Carti - Whole Lotta Red" for User ID 0
+            </button>
+        </>
+    );
+};
