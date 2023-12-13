874f3a2ddf4c5c02655fe049f6e45acc117082a7
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2023-12-12 22:37:38 -0500

    Update user profile page

--- web/src/components/SongTile.jsx
+++ web/src/components/SongTile.jsx
@@ -3,27 +3,30 @@ import { IoMdStarOutline } from "react-icons/io";
 import { IoMdStarHalf } from "react-icons/io";
 import { IoMdStar } from "react-icons/io";
 
-export default () => {
+export default ({rating = false}) => {
     return (
-        <div className="h-80 w-70 rounded-xl border border-accent-vivid/30 bg-accent-neutral/5">
+        <div className="h-80 w-64 rounded-xl border-2  border-secondary/20 bg-secondary/5">
             <Image 
                 src="https://media.pitchfork.com/photos/5eac22c8bae33a8e8fd0b191/1:1/w_450%2Cc_limit/Drake.jpg"
-                width={200}
-                height={200}
-                className="mx-auto mt-6 border border-accent-neutral/20 rounded-xl"
+                width={175}
+                height={175}
+                className="mx-auto mt-6 border border-accent-neutral/5 shadow-xl rounded-xl"
             >
             </Image>
-            <div className="mx-auto place-content-center flex flex-row space-x-2 py-2">
-                <IoMdStar className="text-yellow-500 text-2xl" />
-                <IoMdStar className="text-yellow-500 text-2xl" />
-                <IoMdStar className="text-yellow-500 text-2xl" />
-                <IoMdStar className="text-yellow-500 text-2xl" />
-                <IoMdStarHalf className="text-yellow-500 text-2xl" />
+            <div className="flex flex-col place-content-start pt-4"> 
+                <h1 className="text-xl font-bold text-center text-text">Pain 1993</h1>
+                <span className="text-sm text-center font-light text-text/50">Drake</span>
             </div>
-            <div className="w-full flex flex-row place-content-evenly px-8 pt-2">
-                <h1 className="text-lg font-bold text-center text-text mr-auto mt-auto">Pain 1993</h1>
-                <span className="text-lg text-center font-normal text-text mt-auto">Drake</span>
-            </div>
+            {
+                rating && 
+                <div className="mx-auto place-content-center flex flex-row space-x-0.5 py-2">
+                    <IoMdStar className="text-secondary text-2xl" />
+                    <IoMdStar className="text-secondary text-2xl" />
+                    <IoMdStar className="text-secondary text-2xl" />
+                    <IoMdStar className="text-secondary text-2xl" />
+                    <IoMdStarHalf className="text-secondary text-2xl" />
+                </div>
+            }
         </div>
     );
 };
--- web/src/pages/profile.jsx
+++ web/src/pages/profile.jsx
@@ -1,37 +1,38 @@
 import Image from 'next/image';
+import SongTile from "@/components/SongTile";
 import SideBar from "@/components/SideBar";
 import {useEffect, useState} from "react";
 
 function ReviewTile() {
     return (
-        <div className="rounded-xl h-32 w-64 px-4 py-2 border border-accent-neutral/10 bg-accent-vivid/5 flex flex-col">
-            <h3 className="font-semibold italic">Pain 1993 - Drake</h3>
-            <p className="font-light text-text/80 text-sm">Why lorem text placeholder...</p>
+        <div className="rounded-xl border-2 h-32 w-64 px-4 py-2 border border-secondary/20 bg-secondary/5 flex flex-col">
+            <h3 className="font-bold italic text-text/90">Pain 1993 - Drake</h3>
+            <p className="font-normal text-secondary/90 text-sm">Why lorem text placeholder...</p>
             <div className="mt-auto flex flex-row place-content-between">
-                <p className="font-bold text-text text-md italic">24 Stars</p>
-                <p className="font-bold text-text text-md italic ml-auto">125 Views</p>
+                <p className="font-semibold text-text/80 text-md italic">24 Stars</p>
+                <p className="font-bold text-text/80 text-md italic ml-auto">125 Views</p>
             </div>
         </div>
     )
 }
 
-function PinnedTile() {
-    return (
-        <div className="h-64 w-52 rounded-xl border border-accent-neutral/10 bg-accent-neutral/5">
-            <Image 
-                src="https://media.pitchfork.com/photos/5eac22c8bae33a8e8fd0b191/1:1/w_450%2Cc_limit/Drake.jpg"
-                width={150}
-                height={150}
-                className="mx-auto mt-6 border border-accent-neutral/20 rounded-xl"
-            >
-            </Image>
-            <div className="w-full flex flex-col place-content-evenly px-8 pt-2">
-                <h1 className="text-lg font-bold text-center text-text mt-auto">Pain 1993</h1>
-                <span className="text-lg text-center font-normal text-text mt-auto">Drake</span>
-            </div>
-        </div>
-    );
-};
+{/* function PinnedTile() { */}
+    {/* return ( */}
+        {/* <div className="h-64 w-52 rounded-xl border border-accent-neutral/10 bg-accent-neutral/5"> */}
+            {/* <Image  */}
+                {/* src="https://media.pitchfork.com/photos/5eac22c8bae33a8e8fd0b191/1:1/w_450%2Cc_limit/Drake.jpg" */}
+                {/* width={150} */}
+                {/* height={150} */}
+                {/* className="mx-auto mt-6 border border-accent-neutral/20 rounded-xl" */}
+            {/* > */}
+            {/* </Image> */}
+            {/* <div className="w-full flex flex-col place-content-evenly px-8 pt-2"> */}
+                {/* <h1 className="text-lg font-bold text-center text-text mt-auto">Pain 1993</h1> */}
+                {/* <span className="text-lg text-center font-normal text-text mt-auto">Drake</span> */}
+            {/* </div> */}
+        {/* </div> */}
+    {/* ); */}
+{/* }; */}
 
 export default () => {
     const [name, setName] = useState("Loading...");
@@ -67,10 +68,10 @@ export default () => {
                     <div className="flex flex-row space-x-4">
                         <div className="flex flex-col space-y-2 w-1/6">
                             <Image 
-                                src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                                src="https://media.pitchfork.com/photos/65774786a9bb8a8b8b679df5/1:1/w_320,c_limit/Elzhi%20:%20Oh%20No-%20Heavy%20Vibrato.jpeg"
                                 width={225}
                                 height={225}
-                                className="border border-accent-neutral/20 rounded-xl"
+                                className="border border-accent-neutral/30 rounded-xl"
                             >
                             </Image>
                             <div className="flex flex-row space-x-2" >
@@ -99,13 +100,13 @@ export default () => {
                             </div>
                         </div>
                     </div>
-                    <hr className="w-5/6 mr-auto border-t-2 my-6 border-accent-vivid/30"></hr>
+                    <hr className="w-5/6 mr-auto border-t-2 my-6 border-secondary"></hr>
                     <div>
                         <h2 className="text-2xl font-bold">Pinned Songs</h2>
                         <div className="flex flex-row w-5/6 mx-auto place-content-around pt-5">
-                            <PinnedTile />
-                            <PinnedTile />
-                            <PinnedTile />
+                            <SongTile key={1} />
+                            <SongTile key={2} />
+                            <SongTile key={3} />
                         </div>
                     </div>
                     <div className="pt-5">
