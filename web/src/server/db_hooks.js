1677124ef4c8943207bda9e8c1b17b7501446559
Tim Rolshud <tim.rolsh@gmail.com>
2023-12-13 18:51:03 -0500

    init add db hooks

---
+++ web/src/server/db_hooks.js
@@ -0,0 +1,13 @@
+const db = require("./db_connect");
+
+function getUserInfo() {}
+
+// get the average rating, comments, and replies for a work on spotify (music)
+function getWorkInfo() {}
+
+
+
+module.exports = {
+    getUserInfo: getUserInfo,
+    getWorkInfo: getWorkInfo
+};
