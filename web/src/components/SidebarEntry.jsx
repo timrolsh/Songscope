d4285d10e078670266be4eaf1bbed2c0797030cc
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2023-12-14 09:15:48 -0500

    UI: Revamp Sidebar Colors, Add Modal Support/Hover Animations (#35)
    * Update Dashboard sidebar
    
    * Revamp sidebar to more modern look
    Removed unnecessary "sign-in" file
    
    * Update user profile page
    
    * Add hover effect to SongTiles
    
    * Add in song modal, overhaul tile hover animation
    Generally, make SongTile not look horrible
    
    * Minor adjustments to profile page
    
    * format readme
    
    ---------
    
    Co-authored-by: Tim Rolshud <tim.rolsh@gmail.com>

--- web/src/components/SidebarEntry.jsx
+++ web/src/components/SidebarEntry.jsx
@@ -2,9 +2,9 @@ export default () => {
     
     return (
         // TODO --> on hover, make it so that you cannot highlight/select text?
-        <div className="flex flex-row place-content-before hover:bg-accent-neutral/20 transition px-2.5 py-1.5 rounded-md"> 
-            <div className="w-5 h-5 bg-primary my-auto"></div>
-            <h3 className="pl-4 my-auto text-lg font-normal"> Pain 1993 - <span className="italic">Drake</span></h3>
+        <div className="flex flex-row place-content-before hover:bg-accent-neutral/20 transition px-2.5 py-1.5 my-0.5 rounded-md"> 
+            <div className="w-5 h-5 bg-primary/20 my-auto"></div>
+            <h3 className="pl-2 my-auto text-sm font-normal text-text/90"> Pain 1993 - <span className="italic">Drake</span></h3>
         </div>
     );
 };
--- web/src/pages/profile.jsx
+++ web/src/pages/profile.jsx
@@ -1,37 +1,38 @@
 import Image from 'next/image';
+import SongTile from "@/components/SongTile";
 import SideBar from "@/components/SideBar";
 import {useEffect, useState} from "react";
 
 function ReviewTile() {
     return (
-        <div className="rounded-xl h-32 w-64 px-4 py-2 border border-accent-neutral/10 bg-accent-vivid/5 flex flex-col">
-            <h3 className="font-semibold italic">Pain 1993 - Drake</h3>
-            <p className="font-light text-text/80 text-sm">Why lorem text placeholder...</p>
+        <div className="rounded-xl h-32 w-64 px-4 py-2 border border-secondary/20 bg-secondary/5 flex flex-col">
+            <h3 className="font-bold italic text-text/90">Pain 1993 - Drake</h3>
+            <p className="font-normal text-secondary/90 text-sm">Why lorem text placeholder...</p>
             <div className="mt-auto flex flex-row place-content-between">
-                <p className="font-bold text-text text-md italic">24 Stars</p>
-                <p className="font-bold text-text text-md italic ml-auto">125 Views</p>
+                <p className="font-semibold text-text/80 text-md italic">24 Stars</p>
+                <p className="font-bold text-text/80 text-md italic ml-auto">125 Views</p>
             </div>
         </div>
     )
 }
 
-function PinnedTile() {
-    return (
-        <div className="h-64 w-52 rounded-xl border border-accent-neutral/10 bg-accent-neutral/5">
-            <Image 
-                src="https://media.pitchfork.com/photos/5eac22c8bae33a8e8fd0b191/1:1/w_450%2Cc_limit/Drake.jpg"
-                width={150}
-                height={150}
-                className="mx-auto mt-6 border border-accent-neutral/20 rounded-xl"
-            >
-            </Image>
-            <div className="w-full flex flex-col place-content-evenly px-8 pt-2">
-                <h1 className="text-lg font-bold text-center text-text mt-auto">Pain 1993</h1>
-                <span className="text-lg text-center font-normal text-text mt-auto">Drake</span>
-            </div>
-        </div>
-    );
-};
+{/* function PinnedTile() { */}
+    {/* return ( */}
+        {/* <div className="h-64 w-52 rounded-xl border border-accent-neutral/10 bg-accent-neutral/5"> */}
+            {/* <Image  */}
+                {/* src="https://media.pitchfork.com/photos/5eac22c8bae33a8e8fd0b191/1:1/w_450%2Cc_limit/Drake.jpg" */}
+                {/* width={150} */}
+                {/* height={150} */}
+                {/* className="mx-auto mt-6 border border-accent-neutral/20 rounded-xl" */}
+            {/* > */}
+            {/* </Image> */}
+            {/* <div className="w-full flex flex-col place-content-evenly px-8 pt-2"> */}
+                {/* <h1 className="text-lg font-bold text-center text-text mt-auto">Pain 1993</h1> */}
+                {/* <span className="text-lg text-center font-normal text-text mt-auto">Drake</span> */}
+            {/* </div> */}
+        {/* </div> */}
+    {/* ); */}
+{/* }; */}
 
 export default () => {
     const [name, setName] = useState("Loading...");
@@ -62,27 +63,29 @@ export default () => {
     return (
         <div className="flex flex-row h-full">
             <SideBar variant={"profile"}/> 
-            <div className="w-4/5 h-screen overflow-auto">
+            <div className="w-4/5 sm:w-5/6 h-screen overflow-auto">
                 <div className="pt-8 px-8">
                     <div className="flex flex-row space-x-4">
                         <div className="flex flex-col space-y-2 w-1/6">
                             <Image 
-                                src="https://t4.ftcdn.net/jpg/00/64/67/63/360_F_64676383_LdbmhiNM6Ypzb3FM4PPuFP9rHe7ri8Ju.jpg"
+                                src="https://media.pitchfork.com/photos/65774786a9bb8a8b8b679df5/1:1/w_320,c_limit/Elzhi%20:%20Oh%20No-%20Heavy%20Vibrato.jpeg"
                                 width={225}
                                 height={225}
-                                className="border border-accent-neutral/20 rounded-xl"
+                                className="border border-accent-neutral/30 rounded-xl"
                             >
                             </Image>
                             <div className="flex flex-row space-x-2" >
                                 {/* <div className="w-5 h-5 rounded-2xl bg-lime-400 mb-auto"></div> */}
                             </div>
                         </div>
-                        <div className="w-1/2 text-lg">
-                            <h1 className="font-bold text-2xl mx-auto pb-4">{name}</h1>
-                            <h3>I love music, and I love sharing my thoughts with the world! Everyone should know what I think about all the songs, and I have the best music taste ever!</h3>
-                            <h3 className="text-text/50 italic font-light pt-8">Scoping out songs since: Nov 2, 2023</h3>
+                        <div className="w-1/2 sm:w-4/6 text-lg flex flex-col place-content-between">
+                            <div>
+                                <h1 className="font-bold text-2xl mx-auto">{name}</h1>
+                                <h3>I love music, and I love sharing my thoughts with the world! Everyone should know what I think about all the songs, and I have the best music taste ever!</h3>
+                            </div>
+                            <h3 className="text-text/50 italic font-light sm:pb-1">Scoping out songs since: Nov 2, 2023</h3>
                         </div>
-                        <div className="border-l-2 border-l-accent-neutral/20 pl-6 w-2/6">
+                        <div className="border-l-2 border-l-accent-neutral/20 pl-6 w-2/6 sm:w-1/6">
                             <h1 className="mr-auto font-semibold text-xl pb-3">Lifetime Stats</h1>
                             <div className="flex flex-col space-y-2">
                                 <div className="flex flex-row space-x-4">
@@ -97,18 +100,19 @@ export default () => {
                             </div>
                         </div>
                     </div>
-                    <hr className="w-5/6 mr-auto border-t-2 my-6 border-accent-vivid/30"></hr>
+                    <hr className="w-7/8 mr-auto border-t-2 my-6 border-secondary"></hr>
                     <div>
-                        <h2 className="text-2xl font-bold">Pinned Songs</h2>
-                        <div className="flex flex-row w-5/6 mx-auto place-content-around pt-5">
-                            <PinnedTile />
-                            <PinnedTile />
-                            <PinnedTile />
+                        <h2 className="text-2xl pl-2 font-bold">Pinned Songs</h2>
+                        <div className="flex flex-row w-full mx-auto place-content-between pt-5">
+                            <SongTile key={1} />
+                            <SongTile key={2} />
+                            <SongTile key={3} />
+                            <SongTile key={4} />
                         </div>
                     </div>
                     <div className="pt-5">
-                        <h2 className="text-2xl font-bold">Top Reviews</h2>
-                        <div className="flex flex-row w-5/6 mx-auto place-content-around pt-5">
+                        <h2 className="text-2xl pl-2 font-bold">Top Reviews</h2>
+                        <div className="flex flex-row w-full mx-auto place-content-evenly pt-5">
                             <ReviewTile />
                             <ReviewTile />
                             <ReviewTile />
