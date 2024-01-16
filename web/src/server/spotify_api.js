7457bdea0242d9dad5272a88dc71f82eb32beb7e
Lermanator <nerdpebble@gmail.com>
2024-01-16 12:43:36 -0500

    Switch .then/.catch to await syntax throughout project

--- web/src/server/spotify_api.js
+++ web/src/server/spotify_api.js
@@ -7,109 +7,100 @@ var spotifyWebApi = new SpotifyWebApi({
     clientSecret: process.env.SPOTIFY_CLIENT_SECRET
 });
 
-function getAccessToken() {
-    return spotifyWebApi.clientCredentialsGrant().then(
-        function (data) {
-            spotifyWebApi.setAccessToken(data.body["access_token"]);
-            return Promise.resolve(data.body["access_token"]);
-        },
-        function (err) {
-            console.error("Something went wrong when retrieving an access token", err);
-            return Promise.reject(err);
-        }
-    );
+async function getAccessToken() {
+    try {
+        const data = await spotifyWebApi.clientCredentialsGrant();
+        spotifyWebApi.setAccessToken(data.body["access_token"]);
+        return data.body["access_token"];
+    } catch (err) {
+        console.error("Something went wrong when retrieving an access token", err);
+        throw err;
+    }
 }
 
-function executeMethod(method, ...args) {
+async function executeMethod(method, ...args) {
     const boundMethod = method.bind(spotifyWebApi);
-    return boundMethod(...args).catch((err) => {
-        // Token expired or not set
+    try {
+        return await boundMethod(...args);
+    } catch (err) {
         if (err.statusCode === 401) {
-            return getAccessToken().then(() => boundMethod(...args));
+            await getAccessToken();
+            return boundMethod(...args);
         }
         throw err;
-    });
+    }
 }
 
 class SpotifyApi {
-    getAlbum(albumId) {
-        return executeMethod(spotifyWebApi.getAlbum.bind(spotifyWebApi), albumId).then((result) => {
-            let album = {
-                id: result.body.id,
-                name: result.body.name,
-                artist: result.body.artists[0].name,
-                artist_id: result.body.artists[0].id,
-                releaseDate: result.body.release_date,
-                albumArtUrl: result.body.images[0].url
-            };
-            return Promise.resolve(album);
-        });
+    async getAlbum(albumId) {
+        const result = await executeMethod(spotifyWebApi.getAlbum.bind(spotifyWebApi), albumId);
+        return {
+            id: result.body.id,
+            name: result.body.name,
+            artist: result.body.artists[0].name,
+            artist_id: result.body.artists[0].id,
+            releaseDate: result.body.release_date,
+            albumArtUrl: result.body.images[0].url
+        };
     }
 
-    getSong(songId) {
-        return executeMethod(spotifyWebApi.getTrack.bind(spotifyWebApi), songId).then((result) => {
-            let song = {
-                id: result.body.id,
-                name: result.body.name,
-                artist: result.body.artists[0].name,
-                artist_id: result.body.artists[0].id,
-                album: result.body.album.name,
-                album_id: result.body.album.id,
-                albumArtUrl: result.body.album.images[0].url
-            };
-            return Promise.resolve(song);
-        });
+    async getSong(songId) {
+        const result = await executeMethod(spotifyWebApi.getTrack.bind(spotifyWebApi), songId);
+        return {
+            id: result.body.id,
+            name: result.body.name,
+            artist: result.body.artists[0].name,
+            artist_id: result.body.artists[0].id,
+            album: result.body.album.name,
+            album_id: result.body.album.id,
+            albumArtUrl: result.body.album.images[0].url
+        };
     }
 
-    getSongsFromAlbum(albumId) {
-        return executeMethod(spotifyWebApi.getAlbumTracks.bind(spotifyWebApi), albumId).then(
-            (result) => {
-                let songs = [];
-                result.body.items.forEach((song) => {
-                    songs.push({
-                        id: song.id,
-                        name: song.name,
-                        artist: song.artists[0].name,
-                        artist_id: song.artists[0].id,
-                        album: song.album.name,
-                        album_id: song.album.id,
-                        albumArtUrl: song.album.images[0].url
-                    });
-                });
-                return Promise.resolve(songs);
-            }
+    async getSongsFromAlbum(albumId) {
+        const result = await executeMethod(
+            spotifyWebApi.getAlbumTracks.bind(spotifyWebApi),
+            albumId
         );
+        return result.body.items.map((song) => ({
+            id: song.id,
+            name: song.name,
+            artist: song.artists[0].name,
+            artist_id: song.artists[0].id,
+            album: song.album.name,
+            album_id: song.album.id,
+            albumArtUrl: song.album.images[0].url
+        }));
     }
 
-    getSongsFromPlaylist(playlistId) {
-        return executeMethod(spotifyWebApi.getPlaylist.bind(spotifyWebApi), playlistId).then(
-            (result) => {
-                let songs = [];
-                result.body.tracks.items.forEach((song) => {
-                    let albumArtUrl = song.track.album.images.length
-                        ? song.track.album.images[0].url
-                        : "/no-album-cover.jpg";
-                    songs.push({
-                        id: song.track.id,
-                        name: song.track.name,
-                        artist: song.track.artists[0].name,
-                        artist_id: song.track.artists[0].id,
-                        album: song.track.album.name,
-                        album_id: song.track.album.id,
-                        releaseDate: song.track.album.release_date,
-                        albumArtUrl: albumArtUrl,
-                        popularity: song.track.popularity,
-                        previewUrl: song.track.preview_url,
-                        availableMarkets: song.track.available_markets
-                    });
-                });
-                return Promise.resolve(songs);
-            }
+    async getSongsFromPlaylist(playlistId) {
+        const result = await executeMethod(
+            spotifyWebApi.getPlaylist.bind(spotifyWebApi),
+            playlistId
         );
+        return result.body.tracks.items.map((item) => {
+            const song = item.track;
+            let albumArtUrl = song.album.images.length
+                ? song.album.images[0].url
+                : "/no-album-cover.jpg";
+            return {
+                id: song.id,
+                name: song.name,
+                artist: song.artists[0].name,
+                artist_id: song.artists[0].id,
+                album: song.album.name,
+                album_id: song.album.id,
+                releaseDate: song.album.release_date,
+                albumArtUrl: albumArtUrl,
+                popularity: song.popularity,
+                previewUrl: song.preview_url,
+                availableMarkets: song.available_markets
+            };
+        });
     }
 }
 
-// TODO - This is a hacky way to refresh the token every hour, remove and replace with a more robust method
+// Token refresh setup
 setInterval(getAccessToken, 1000 * 60 * 60);
 const spotifyApi = new SpotifyApi();
 
