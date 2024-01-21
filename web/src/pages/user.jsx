8707289aa6e60a3f8c594fe20ae4eba661cc4ca3
Krish Arora <krisharora27@gmail.com>
2024-01-21 14:10:22 -0500

    Update search button styling

--- web/src/pages/user.jsx
+++ web/src/pages/user.jsx
@@ -114,7 +114,7 @@ export default function Home({songsProp}) {
                         onChange={(event) => setSearchQuery(event.target.value)}
                     />
                     <button
-                        className="ml-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        className="ml-4 bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded-md transition"
                         onClick={fetchMoreSongs}
                         disabled={loading}
                     >
