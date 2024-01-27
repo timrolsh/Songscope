b415c2635e39c61be7ae1aa46a764734dc1eab84
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2024-01-27 16:13:29 -0500

    Remove unneeded files

--- web/src/server/start_server.js
+++ web/src/server/start_server.js
@@ -1,6 +1,5 @@
 import express, {json} from "express";
 import next from "next";
-import db from "./db_connect";
 const port = 3000;
 
 const devMode = process.argv[2] === "dev";
@@ -15,27 +14,18 @@ async function startServer() {
         const bodyParser = require("body-parser");
         server.use(bodyParser.urlencoded({extended: true}));
 
-        db.query("select NOW();", (error, results, fields) => {
-            if (error) {
-                console.error(
-                    "SONGSCOPE: Unable to start server. Failed to connect to database",
-                    error
-                );
-                process.exit(1);
+        // Handle both get and post requests with Next.js API system
+        server.get("*", (request, response) => {
+            return nextApp.getRequestHandler()(request, response);
+        });
+        server.post("*", (request, response) => {
+            return nextApp.getRequestHandler()(request, response);
+        });
+        server.listen(port, (err) => {
+            if (err) {
+                throw err;
             }
-            // Handle both get and post requests with Next.js API system
-            server.get("*", (request, response) => {
-                return nextApp.getRequestHandler()(request, response);
-            });
-            server.post("*", (request, response) => {
-                return nextApp.getRequestHandler()(request, response);
-            });
-            server.listen(port, (err) => {
-                if (err) {
-                    throw err;
-                }
-                console.log(`SONGSCOPE: Started server on http://localhost:${port}`);
-            });
+            console.log(`SONGSCOPE: Started server on http://localhost:${port}`);
         });
     } catch (error) {
         console.error("SONGSCOPE: Unable to start server.", error);
