df7c0e3faa83715bc3263370ba5ba29a9fbef910
Tim Rolshud <tim.rolsh@gmail.com>
2024-01-29 16:03:59 -0500

    Add ResetSchema.sql file, isolate from main Schema creation file

---
+++ db/ResetSchema.sql
@@ -0,0 +1,23 @@
+/*
+This file wipes all associated tables for Songscope from the database. 
+To reset the database, run this script and then run the Schema.sql 
+file to reset the database with empty data. 
+*/
+
+-- Use our assigned schema
+USE capstone_2324_songscope;
+
+-- disable foreign key checks so that tables could be dropped
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- Drop each table individually if they exist
+DROP TABLE IF EXISTS accounts cascade;
+DROP TABLE IF EXISTS sessions cascade;
+DROP TABLE IF EXISTS users cascade;
+DROP TABLE IF EXISTS comment cascade;
+DROP TABLE IF EXISTS rating cascade;
+DROP TABLE IF EXISTS comment cascade;
+DROP TABLE IF EXISTS verification_token cascade;
+
+-- re-enable foreign key checks for normal functionality
+SET FOREIGN_KEY_CHECKS = 1;
