704243a26ab06699d914dc6c1410dc753d6d7fee
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2024-02-04 19:42:06 -0500

    Overhaul audio player (#53)
    * Overhaul audio player
    Formatting as well
    
    * reformat package lock

--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,6 +1,6 @@
 import {Html, Head, Main, NextScript} from "next/document";
 
-export default (): JSX.Element =>  {
+export default (): JSX.Element => {
     return (
         <Html lang="en">
             <Head />
@@ -10,4 +10,4 @@ export default (): JSX.Element =>  {
             </body>
         </Html>
     );
-}
+};
