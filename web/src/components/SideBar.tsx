0e7d06ebfbe4a2f2a527a11161e4d75e3625b39f
Krish Arora <krisharora27@gmail.com>
2024-02-22 01:34:50 -0500

    Refactor SideBar component to reflect new settings page

--- web/src/components/SideBar.tsx
+++ web/src/components/SideBar.tsx
@@ -2,18 +2,13 @@ import Link from "next/link";
 import SidebarEntry from "./SidebarEntry";
 import {signOut} from "next-auth/react";
 import {SongMetadata} from "@/types";
-import {MdOutlineSecurity} from "react-icons/md";
-import {MdOutlineCompareArrows} from "react-icons/md";
-import {MdInfoOutline} from "react-icons/md";
-import {HiOutlineCog8Tooth} from "react-icons/hi2";
+
 import Spinner from "@/components/Spinner";
 import {useEffect, useState} from "react";
 import {EnrichedSongMetadata} from "@/pages/api/db/get-top-reviewed";
 const LinkStyles =
     "w-full rounded-md border bg-accent-neutral/5 border-accent-neutral/10 hover:bg-accent-neutral/20 pl-4 \
     transition py-1 hover:-translate-y-0.5 aria-disabled:cursor-default aria-disabled:opacity-50 aria-disabled:hover:translate-y-0 aria-disabled:hover:bg-accent-neutral/5";
-const SettingsLinkStyles =
-    "rounded-lg hover:bg-accent-neutral/50 transition-all py-2 hover:font-semibold hover:-translate-y-0.5 text-text/90 hover:text-text text-sm font-normal px-2 flex flex-row space-x-2";
 
 // TODO --> Migrate this to slot architecture for better reusability
 function renderDashboardBody() {
@@ -27,6 +22,7 @@ function renderDashboardBody() {
             try {
                 const hotResponse = await fetch("/api/db/get-hot-reviewed");
                 const hotSongsData = await hotResponse.json();
+                console.log(hotResponse)
                 setHotSongs(hotSongsData);
 
                 const topResponse = await fetch("/api/db/get-top-reviewed");
@@ -47,7 +43,7 @@ function renderDashboardBody() {
 
     return (
         <>
-            {topSongs.length > 0 && (
+            {topSongs && (
                 <div className="flex flex-col">
                     <h3 className="text-text/90 text-xl font-semibold pb-2">Top Songs</h3>
                     <div className="flex flex-col overflow-y-scroll">
@@ -57,7 +53,7 @@ function renderDashboardBody() {
                     </div>
                 </div>
             )}
-            {hotSongs.length > 0 && (
+            {hotSongs && (
                 <>
                     <hr className="border-t-2 border-accent-neutral/20 mt-5 mb-4"></hr>
                     <h3 className="text-text/90 text-xl font-semibold pb-2">Hot Reviews</h3>
@@ -68,31 +64,6 @@ function renderDashboardBody() {
                     </div>
                 </>
             )}
-        </>
-    );
-}
-
-function renderSettingsBody() {
-    return (
-        <>
-            <div className="flex flex-col">
-                <Link className={SettingsLinkStyles} href="/settings/general">
-                    {" "}
-                    <HiOutlineCog8Tooth className="my-auto text-xl mr-3" /> General
-                </Link>
-                <Link className={SettingsLinkStyles} href="">
-                    {" "}
-                    <MdOutlineCompareArrows className="my-auto text-xl mr-3" /> Integrations
-                </Link>
-                <Link className={SettingsLinkStyles} href="">
-                    <MdOutlineSecurity className="my-auto text-xl mr-3" /> Privacy
-                </Link>
-                <Link className={SettingsLinkStyles} href="">
-                    {" "}
-                    <MdInfoOutline className="my-auto text-xl mr-3" />
-                    Preferences
-                </Link>
-            </div>
         </>
     );
 }
@@ -140,7 +111,7 @@ export default ({
             {variant == "profile"
                 ? renderProfileBody(favoriteSongs)
                 : variant == "settings"
-                  ? renderSettingsBody()
+                  ? <></>
                   : renderDashboardBody()}
             <hr className="border-t-2 border-accent-neutral/20 mt-auto mb-4"></hr>
             <div className="w-full h-52 flex flex-col mb-4">
@@ -166,7 +137,7 @@ export default ({
                     )}
                     <Link
                         className={LinkStyles}
-                        href="/settings/general"
+                        href="/settings"
                         aria-disabled={variant === "settings"}
                     >
                         User Settings
