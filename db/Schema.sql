ebcca9681111f09357710981ab082e90b4144884
Tim Rolshud <tim.rolsh@gmail.com>
2024-04-11 12:19:27 -0400

    Modify schema to support cascading upon user deletion

--- db/Schema.sql
+++ db/Schema.sql
@@ -2,42 +2,6 @@
 USE capstone_2324_songscope;
 
 /*
-Each row in this table represents a link between a user and a third party provider, such as Spotify or Google.
-One user can have multiple entries in this table for different providers that they are authenticated with.
-An auth account is tied to a user account, and for each provider, there can only be one account (these are 2 constraints).
-*/
-create table if not exists accounts
-(
-    id                bigint unsigned auto_increment not null primary key,
-    userId            int                            not null,
-    type              varchar(255)                   not null,
-    provider          varchar(255)                   not null,
-    providerAccountId varchar(255)                   not null,
-    refresh_token     text                           null,
-    access_token      text                           null,
-    expires_at        bigint                         null,
-    id_token          text                           null,
-    scope             text                           null,
-    session_state     text                           null,
-    token_type        text                           null,
-    constraint accounts_pk
-        unique (provider, providerAccountId),
-    constraint accounts_users_id_fk
-        foreign key (userId) references users (id)
-);
-
-create table if not exists sessions
-(
-    id           bigint unsigned auto_increment
-        primary key,
-    userId       int          not null,
-    expires      timestamp    not null,
-    sessionToken varchar(255) not null,
-    constraint id
-        unique (id)
-);
-
-/*
 A user's primary account for the platform.
 */
 create table if not exists users
@@ -60,6 +24,42 @@ create table if not exists users
         unique (id)
 );
 
+/*
+Each row in this table represents a link between a user and a third party provider, such as Spotify or Google.
+One user can have multiple entries in this table for different providers that they are authenticated with.
+An auth account is tied to a user account, and for each provider, there can only be one account (these are 2 constraints).
+*/
+create table if not exists accounts
+(
+    id                bigint unsigned auto_increment not null primary key,
+    userId            bigint unsigned                not null,
+    type              varchar(255)                   not null,
+    provider          varchar(255)                   not null,
+    providerAccountId varchar(255)                   not null,
+    refresh_token     text                           null,
+    access_token      text                           null,
+    expires_at        bigint                         null,
+    id_token          text                           null,
+    scope             text                           null,
+    session_state     text                           null,
+    token_type        text                           null,
+    constraint accounts_pk
+        unique (provider, providerAccountId),
+    constraint accounts_users_id_fk
+        foreign key (userId) references users (id) on delete cascade
+);
+
+create table if not exists sessions
+(
+    id           bigint unsigned auto_increment
+        primary key,
+    userId       int          not null,
+    expires      timestamp    not null,
+    sessionToken varchar(255) not null,
+    constraint id
+        unique (id)
+);
+
 create table if not exists comment
 (
     id              int auto_increment
@@ -69,7 +69,7 @@ create table if not exists comment
     comment_text    text                                not null,
     time            timestamp default CURRENT_TIMESTAMP not null,
     constraint comment_users_id_fk
-        foreign key (user_id) references users (id)
+        foreign key (user_id) references users (id) on delete cascade
 );
 
 create table if not exists user_comment
@@ -95,7 +95,7 @@ create table if not exists user_song
     pinned          tinyint(1) default 0 not null,
     primary key (user_id, spotify_work_id),
     constraint user_song_users_id_fk
-        foreign key (user_id) references users (id)
+        foreign key (user_id) references users (id) on delete cascade
 );
 
 create table if not exists verification_token
