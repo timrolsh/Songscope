4cfed482d9fe6184b156ca85685be1d08e29b558
Tim Rolshud <tim.rolsh@gmail.com>
2024-05-21 20:02:14 -0400

    Fix error in session user type

--- web/src/nextauth.d.ts
+++ web/src/nextauth.d.ts
@@ -3,6 +3,6 @@ import NextAuth from "next-auth";
 import {User} from "./types";
 declare module "next-auth" {
     interface Session {
-        user: User;
+        user: User & DefaultSession["user"];
     }
 }
