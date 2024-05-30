d6c2c653ae67e321ea5c63d9b13b6bbf452c01df
Yusuf Sallam <68395788+yusufsallam64@users.noreply.github.com>
2024-05-29 23:05:31 -0400

    Attempt to fix offset for comment times on prod

--- web/src/dates.ts
+++ web/src/dates.ts
@@ -4,7 +4,7 @@ import {DateTime} from "luxon";
 export function CommentTimestamp(timestamp: string): string {
     const date = DateTime.fromSQL(timestamp.split("T").join(" "), {zone: "utc"})
         .setZone("local")
-        .minus({hours: 4});
+        // .minus({hours: 4});
     return date.toRelative() as string;
 }
 
